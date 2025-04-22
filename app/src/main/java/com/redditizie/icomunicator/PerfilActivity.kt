b7b20dc15aa5c71534909962765efdb41dc84726
Rodrigo <redditizie@gmail.com>
2025-04-22 22:58:41 +0200

    Feat: add user profile interface layout

--- app/src/main/java/com/redditizie/icomunicator/PerfilActivity.kt
+++ app/src/main/java/com/redditizie/icomunicator/PerfilActivity.kt
@@ -5,12 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.redditizie.icomunicator.databinding.ActivityCadastroBinding
+import com.redditizie.icomunicator.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+
+        ActivityPerfilBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        setContentView(binding.root)
 
+        inicializarToolbar()
+
+    }
+
+    fun inicializarToolbar(){
+
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        setSupportActionBar( toolbar )
+        supportActionBar?.apply {
+            title = "Editar perfil"
+            setDisplayHomeAsUpEnabled(true) // Exibi um botao voltar na tooltbar. Configurar no manifest o nome da activity pai: android:parentActivityName=".LoginActivity"
+        }
     }
 }
